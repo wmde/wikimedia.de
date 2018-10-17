d359910c2b9a23c0c58e0e9f83ef172a6a447705
Florian Stolzenhain <info@stolzenhain.net>
2018-10-17 11:10:29 +0200

    Team: JS: script draft

---
+++ public/js/components/wdiv-team.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// iterate over component
+// TODO: init via closure lib
+// TODO: init once for document context
+// TODO: domReady for more secure init?
+Array.prototype.forEach.call( document.querySelectorAll('.wdiv-team-wrapper') , function (teamWrapper) {
+
+    // TODO: callbacks on filter
+    // TODO: init List.js on content
+    // TODO: single filter function
+    // TODO: multifilter function
+
+    console.log(teamWrapper);
+
+});
