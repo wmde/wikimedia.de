c30541bcedc7118c218c7faea59d4fbf71161a5f
Florian Stolzenhain <info@stolzenhain.net>
2018-10-31 14:43:22 +0100

    Template-Helper: Adding route for team template

--- src/Controller/TemplatesController.php
+++ src/Controller/TemplatesController.php
@@ -13,11 +13,20 @@ use Symfony\Component\Yaml\Yaml;
 class TemplatesController extends Controller {
 
 	public function previewTopics( Request $request ): Response {
-			$path = '/pages/topics-draft';
+		$path = '/pages/topics-draft';
 		$preview = Yaml::parse( file_get_contents(__DIR__.'/../../templates'.$path.'.html.yaml') );
 
 		// we're assuming a data pattern w/ keys `template` and `data` on root
 		return $this->render( $preview['template'], $preview['data'] );
 	}
+
+	public function previewTeam( Request $request ): Response {
+		$path = '/pages/team-draft';
+		$preview = Yaml::parse( file_get_contents(__DIR__.'/../../templates'.$path.'.html.yaml') );
+
+		// we're assuming a data pattern w/ keys `template` and `data` on root
+		return $this->render( $preview['template'], $preview['data'] );
+	}
+
 
 }
