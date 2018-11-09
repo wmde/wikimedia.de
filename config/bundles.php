13cf6a50b71dc484548f2e36fbeb6456235ea486
Florian Stolzenhain <info@stolzenhain.net>
2018-11-09 12:50:15 +0100

    Composer: Version all files modified after composer install
    This hopefully fixes problems on dev

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     JMS\I18nRoutingBundle\JMSI18nRoutingBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
