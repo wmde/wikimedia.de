678d5ff5e171ffe4eca8da76ebc43b6ee0dc5242
Florian Stolzenhain <info@stolzenhain.net>
2018-11-14 17:02:48 +0100

    JS: Nav: mobile toggling
    big technical debt for 2 lines of code …

---
+++ public/js/components/wdiv-header.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// iterate over component
+// TODO: init via closure lib
+// TODO: init once for document context
+// TODO: domReady for more secure init?
+
+var context = document;
+Array.prototype.forEach.call( context.querySelectorAll('.wdiv-header-nav') , function (navWrapper) {
+
+
+    // TODO: get and write nav height in px for animation
+    // TODO: get and write nav height in px for animation
+
+    Array.prototype.forEach.call( navWrapper.querySelectorAll('.wdiv-nav-toggle') , function (button) {
+
+        button.addEventListener('click', function (change) {
+            // Some notes:
+            // - `click` events may prove troublesome on native Android Webview
+            // - referencing `navwrapper` from inside the event could bind the wrong wrapper
+
+            // TODO: get parent element from inside click and parse for
+            //       '.wdiv-header-nav .wdiv-nav-toggle' only
+
+            // TODO: toggle() not supported by IE11
+            navWrapper.classList.toggle('wdiv-js-toggled');
+
+        });
+
+
+    });
+
+});
