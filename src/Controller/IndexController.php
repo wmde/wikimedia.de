b87c0473dde53fb648e86344cade980b8bf09cbe
Florian Stolzenhain <info@stolzenhain.net>
2018-11-22 14:25:15 +0100

    Home: Controller: If Data file is missing, pass empty string
    This allows the data parser to return an empty
    result.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -19,7 +19,11 @@ class IndexController extends BaseController {
 		// thanks to Stackoverflow user Sarfraz we quickly got the concept of dynamic classes, see:
 		// https://stackoverflow.com/a/2350948
 		$datasets = new DatasetPageController;
-		$staffCsv = file_get_contents( $this->container->getParameter( 'kernel.project_dir' ).'/public/files/staff.csv' );
+		function readFile( $filename ) {
+			if (! file_exists( $filename ) ) { return ''; } // no file? return empty string
+			return file_get_contents( $filename );
+		}
+		$staffCsv = readFile( $this->container->getParameter( 'kernel.project_dir' ).'/public/files/staff.csv' );
 
 		return $this->render(
 			'pages/home.html.twig',
