ad614a0e38a00513994ee7bf6f710a79f04503d6
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-12-23 08:10:57 +0100

    CS in IndexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,29 +9,30 @@ use App\Presenter\NewsItemsTwigPresenter;
 // phpcs:ignoreFile
 class IndexController extends BaseController {
 
-	private function readFile( string $filename ): string {
-		if (! file_exists( $filename ) ) { return ''; } // no file? return empty string
-		return file_get_contents( $filename );
-	}
-
 	public function index() {
 		$newsPresenter = new NewsItemsTwigPresenter();
 
 		$newsPresenter->present( $this->getFactory()->newNewsRepository()->getLatestNewsItems() );
 
-		// thanks to Stackoverflow user Sarfraz we quickly got the concept of dynamic classes, see:
-		// https://stackoverflow.com/a/2350948
-		$datasets = new DatasetPageController();
-
-		$staffCsv = $this->readFile( $this->container->getParameter( 'kernel.project_dir' ).'/public/files/staff.csv' );
+		$staffCsv = $this->readFile(
+			$this->container->getParameter( 'kernel.project_dir' ) . '/public/files/staff.csv'
+		);
 
 		return $this->render(
 			'pages/home.html.twig',
 			[
 				'news' => $newsPresenter->getViewModel(),
-				'staff' => $datasets->peopleData( $staffCsv, true )
+				'staff' => ( new DatasetPageController() )->peopleData( $staffCsv, true )
 			]
 		);
+	}
+
+	private function readFile( string $filename ): string {
+		if ( !file_exists( $filename ) ) {
+			return '';
+		}
+
+		return file_get_contents( $filename );
 	}
 
 }
